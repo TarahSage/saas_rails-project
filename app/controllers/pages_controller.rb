e970442e16e0bf549f4f82248d400c610968135a
Tarah Nixon <erraticcreation@gmail.com>
2016-01-21 01:51:34 +0000

    Added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
-    def home
-    end
-    
-    def about
-    end
+  def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
+  end
+  
+  def about
+  end
 end

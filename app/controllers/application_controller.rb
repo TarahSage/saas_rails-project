d5be838999de2028bafcce4b966aa0efe2fc3fd8
Tyler Nixon <ErraticCreation@gmail.com>
2016-01-21 16:57:44 +0000

    fixed typos in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, stripe_card_token, :email, :password, password_confirmation) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
     end
 end

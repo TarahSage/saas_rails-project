3ddfb98985d9137343daaac86a21bfc83fa18fd8
Tyler Nixon <ErraticCreation@gmail.com>
2016-01-21 16:47:52 +0000

    Fixed typos and closed the if statement that wasn't closed in Registrations Controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,15 @@
-class Users::RegistrationssController < Devise::RegistrationssController
+class Users::RegistrationsController < Devise::RegistrationsController
   
   def create
     super do |resource|
       if params[:plan]
-        resource.olan_id = params[:plan]
+        resource.plan_id = params[:plan]
         if resource.plan_id == 2
           resource.save_with_payment
         else
           resource.save
         end
+      end  
     end
   end
-  
 end

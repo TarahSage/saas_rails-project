36bddc7ec804f85e7d7e0b2eafd434bf43c812bf
Tyler Nixon <ErraticCreation@gmail.com>
2016-01-22 14:38:35 +0000

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,7 @@
 class ProfilesController < ApplicationController
-  
+  def new
+    # Form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
+  end
 end
